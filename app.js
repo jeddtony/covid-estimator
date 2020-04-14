ad313641880b7c94d5463b2a45883a6341199220
jeddtony <jedtony.jt@gmail.com>
2020-04-14 21:15:45 +0100

    Add response for xml

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const bodyParser = require('body-parser');
+const xml = require('xml');
+
 const Impact = require('./src/impact/impact');
 const SevereImpact = require('./src/severeImpact/severeImpact');
 
@@ -16,7 +18,7 @@ app.get('/', (req, res, next) => {
 
 
 app.post('/test', async (req, res, next) => {
-  console.log (req.body);
+  // console.log (req.body);
 
   let {
     name,
@@ -69,6 +71,66 @@ app.post('/test', async (req, res, next) => {
   });
 });
 
+
+app.post('/xml', async (req, res, next) => {
+  // console.log (req.body);
+
+  let {
+    name,
+    avgAge,
+    avgDailyIncomeInUSD,
+    avgDailyIncomePopulation
+  } = req.body.region;
+
+
+  let periodType = req.body.periodType;
+  let timeToElapse = req.body.timeToElapse;
+  let reportedCases = req.body.reportedCases;
+  let totalHopspitalBeds = req.body.totalHospitalBeds;
+
+  let impact = new Impact(
+    reportedCases,
+    timeToElapse,
+    totalHopspitalBeds,
+    avgDailyIncomeInUSD,
+    avgDailyIncomePopulation
+  );
+  let severeImpact = new SevereImpact(
+    reportedCases,
+    timeToElapse,
+    totalHopspitalBeds,
+    avgDailyIncomeInUSD,
+    avgDailyIncomePopulation
+  );
+
+  let xmlResponse = xml({
+    estimate: [{
+      impact:[ 
+          {currentlyInfected: impact.getCurrentlyInfected()},
+            {infectionsByRequestedTime: impact.getInfectionsByRequestedTime()}, 
+            {severeCasesByRequestedTime: impact.getSevereCasesByRequestedTime()},
+            {hospitalBedsByRequestedTime: impact.getHospitalBedByRequestedTime()},
+            {casesForICUByRequestedTime: impact.getCasesForICUByRequestTime()},
+            {dollarsInFlight: impact.getDollarsInFlight()}
+          
+      ]},
+    {severeImpact: [
+        {currentlyInfected: severeImpact.getCurrentlyInfected()},
+        {infectionsByRequestedTime: severeImpact.getInfectionsByRequestedTime()}, 
+        {severeCasesByRequestedTime: severeImpact.getSevereCasesByRequestedTime()},
+        {hospitalBedsByRequestedTime: severeImpact.getAvailableBeds()},
+        {casesForICUByRequestedTime: impact.getCasesForICUByRequestTime()},
+        {dollarsInFlight: impact.getDollarsInFlight()}
+    ]
+    }]
+  });
+
+  let testXml = xml({nested: [{ keys: [{ fun: 'hi' }]}]});
+  console.log('showing the xml response ', xmlResponse);
+  return res.header('Content-Type','text/xml').send(xmlResponse);
+  // res.setHeader('Content-type', 'application/json');
+  // return res.json();
+});
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`App started on port ${port}.`));
